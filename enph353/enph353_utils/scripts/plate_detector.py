a917bfc9321ebfd87e4f8e7b682fe2dc6c88d079
jacdeng <jackdeng000@gmail.com>
2019-11-19 18:46:36 -0800

    more fixes

--- enph353/enph353_utils/scripts/plate_detector.py
+++ enph353/enph353_utils/scripts/plate_detector.py
@@ -7,6 +7,9 @@ from sensor_msgs.msg import Image
 import random
 
 
+TOP_TO_EXCLUDE = 0.18
+BOT_TO_INCLUDE = 0.09
+
 class DetectPlate:
 
     def __init__(self):
@@ -31,31 +34,43 @@ class DetectPlate:
 
         #get contours of the masked image
         image , contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) 
-        
+
         if len(contours) != 0:
 
             #find contour with the biggest area
             max_center = max(contours, key=cv.contourArea)
+            #cv_image = cv.circle(cv_image, (int(max_center[0][0][0]),int(max_center[0][0][1])), 5, (0, 0, 255) , -1)
 
             #if contours fit correct dimensions, draw contour
             if self.contour_fit(max_center, cv_image):
                 x, y, w, h = cv.boundingRect(max_center)
-                plate_center = plate_center.append(max_center)
+                plate_center.append(max_center)
 
-                cv.drawContours(cv_image, plate_center, 0, (0,255,0), 3)
+                # cv.drawContours(cv_image, plate_center, -1, (0,255,0), 1)
+
+                y_license_plate = int(BOT_TO_INCLUDE*y)
+                y_QR = int(TOP_TO_EXCLUDE*y)
+
+                # license plate
+                cv.rectangle(cv_image, (x,y+h), (x+w,y+h+y_license_plate), (0,0,255))
+                license_plate = cv_image[y+h:y+h+y_license_plate, x:x+w]
+                
+                # spot num
+                cv.rectangle(cv_image, (x,y+y_QR), (x+w,y+h), (255,0,0))
+                spot_num = cv_image[y+y_QR:y+h, x:x+w]
 
                 #crop plate
-                license_plate = cv_image[y:y+h , x:x+w]
-
                 cv.imshow('plate_number', license_plate)
+                cv.imshow('spot number', spot_num)
                 
+                print("CAUGHT IT")
                 #save license plate
-                cv.imwrite('/home/fizzer/enph353_cnn_lab/pictures' + str(random.randint(0,999)) + '.png', license_plate)
+                # cv.imwrite('/home/fizzer/353_pics/license/pictures' + str(random.randint(0,999)) + '.png', license_plate)
+                # cv.imwrite('/home/fizzer/353_pics/spot_num/pictures' + str(random.randint(0,999)) + '.png', spot_num)
 
         cv.imshow('color', cv_image)
         cv.waitKey(5) 
         return plate_center
-
 
         #masks given image into one that only contains the characters on license plate
     def mask_image(self, cv_image):
@@ -80,11 +95,12 @@ class DetectPlate:
 
         x, y, w, h = cv.boundingRect(center)
 
-        width = w > 50 and w < 200
-        height = h > 50 and h < 200
-        area = cv.contourArea(center) > 7000
+        width = w > 60 and w < 180
+        height = h > 60 and h < 180
+        area = cv.contourArea(center) > 9000
+        aspect_ratio = float(h) / w < 1
 
-        if width and height and area:
+        if width and height and area and aspect_ratio:
             return True
         else:
             return False
